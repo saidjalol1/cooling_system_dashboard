069929f0e549b9ae34019029c72590e59c9162ed
saidjalol <saidjalolturakhujayev@gmail.com>
2025-09-28 18:47:00 +0500

    changed some code

--- main.py
+++ main.py
@@ -22,70 +22,6 @@ from api import user_routes, device_routes
 
 app  = FastAPI()
 
-
-# routes
-app.include_router(user_routes.users)
-app.include_router(device_routes.device)
-
-
-# midlware and cors conf
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
-)
-
-# log conf
-@app.middleware("https")
-async def log_request_time(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    logging.info(f"{request.method} {request.url} - {response.status_code} - Took {process_time:.4f} seconds")
-    return response
-
-
-# template and static files conf
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
-# Endpoints
-@app.get("/")
-async def dashboard(request: Request,db : Session = Depends(get_db)):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
-
-
-@app.get("/detail/{id}/page")
-async def detail(id: int,request: Request,db : Session = Depends(get_db)):
-    data = db.query(Device).filter(Device.id == id).first()
-    
-    humidity = db.query(Humidity).filter(Humidity.device_id == data.device_name).all()
-    vibaration = db.query(Vibration).filter(Vibration.device_id == data.device_name).all()
-    temperature = db.query(Heat).filter(Heat.device_id == data.device_name).all()
-
-    device = jsonable_encoder(data)
-
-    device["temperature"] = jsonable_encoder(temperature)
-    device["vibration"] = jsonable_encoder(vibaration)
-    device["humidity"] = jsonable_encoder(humidity)
-    return templates.TemplateResponse(
-        "motor-detail.html",
-        {"request": request, "context": device}
-    )
-
-
-@app.post("/token")
-async def login(user_token : OAuth2PasswordRequestForm = Depends(),database : Session = Depends(get_db)):
-    user = auth_main.authenticate_user(user_token.username,user_token.password, database)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Could not validated the User")
-    created_token = token.create_access_token(user.username, user.id, timedelta(minutes=1000))
-    return {"access_token": created_token, "token_type": "bearer","user":user}
-
-
 active_connections: list[WebSocket] = []
 
 @app.websocket("/ws/motors")
@@ -135,3 +71,67 @@ async def websocket_motors(websocket: WebSocket):
                     active_connections.remove(conn)
     except WebSocketDisconnect:
         active_connections.remove(websocket)
+
+# routes
+app.include_router(user_routes.users)
+app.include_router(device_routes.device)
+
+
+# midlware and cors conf
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+# log conf
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logging.info(f"{request.method} {request.url} - {response.status_code} - Took {process_time:.4f} seconds")
+    return response
+
+
+# template and static files conf
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+# Endpoints
+@app.get("/")
+async def dashboard(request: Request,db : Session = Depends(get_db)):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+@app.get("/detail/{id}/page")
+async def detail(id: int,request: Request,db : Session = Depends(get_db)):
+    data = db.query(Device).filter(Device.id == id).first()
+    
+    humidity = db.query(Humidity).filter(Humidity.device_id == data.device_name).all()
+    vibaration = db.query(Vibration).filter(Vibration.device_id == data.device_name).all()
+    temperature = db.query(Heat).filter(Heat.device_id == data.device_name).all()
+
+    device = jsonable_encoder(data)
+
+    device["temperature"] = jsonable_encoder(temperature)
+    device["vibration"] = jsonable_encoder(vibaration)
+    device["humidity"] = jsonable_encoder(humidity)
+    return templates.TemplateResponse(
+        "motor-detail.html",
+        {"request": request, "context": device}
+    )
+
+
+@app.post("/token")
+async def login(user_token : OAuth2PasswordRequestForm = Depends(),database : Session = Depends(get_db)):
+    user = auth_main.authenticate_user(user_token.username,user_token.password, database)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Could not validated the User")
+    created_token = token.create_access_token(user.username, user.id, timedelta(minutes=1000))
+    return {"access_token": created_token, "token_type": "bearer","user":user}
+
+
